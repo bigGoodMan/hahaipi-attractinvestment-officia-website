1bbedce6c3f82e2c3596d889b635db4eccb1bd42
hhf <1170533231@qq.com>
2019-05-31 19:22:57 +0800

    图片路径

--- build/rules.js
+++ build/rules.js
@@ -2,13 +2,53 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const devEnv = process.env.NODE_ENV === 'development'
 module.exports = [
   {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  },
+  {
+    test: /\.(png|jpg|gif|jpeg)$/,
+    use: [{
+      // 需要下载file-loader和url-loader
+      loader: 'url-loader',
+      options: {
+        limit: 5 * 1024, // 小于这个时将会已base64位图片打包处理
+        outputPath: 'img', // 图片文件输出的文件夹
+        name: '[name].[ext]'
+        // publicPath: devEnv ? './img' : './img' // 浏览器图片加载路径
+      }
+    }]
+  },
+  {
+    test: /\.html$/,
+    // html中的img标签
+    // use: ['html-withimg-loader']
+    use: {
+      loader: 'html-loader',
+
+      options: {
+        // ignoreCustomFragments: [],
+
+        // root: path.resolve(__dirname, 'src'),
+
+        // attrs: ['img:src']
+
+      }
+    }
+  },
+  {
     test: /\.(css|styl|stylus)$/,
     use: [devEnv ? 'style-loader' : {
       loader: MiniCssExtractPlugin.loader, // 防止css重复打包
       options: {
         // you can specify a publicPath here
         // by default it uses publicPath in webpackOptions.output
-        publicPath: './'
+        publicPath: '../'
         // hmr: devEnv // Hot Module Reloading
       }
     }, {
@@ -27,34 +67,6 @@ module.exports = [
       loader: 'stylus-loader' // compiles stylus to CSS
     }
     ]
-  },
-  {
-    test: /\.html$/,
-    // html中的img标签
-    use: ['html-withimg-loader']
-  },
-  {
-    test: /\.js$/,
-    exclude: /(node_modules|bower_components)/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env']
-      }
-    }
-  },
-  {
-    test: /\.(png|jpg|gif|jpeg)$/,
-    use: [{
-      // 需要下载file-loader和url-loader
-      loader: 'url-loader',
-      options: {
-        limit: 5 * 1024, // 小于这个时将会已base64位图片打包处理
-        outputPath: 'img', // 图片文件输出的文件夹
-        name: '[name].[ext]',
-        publicPath: devEnv ? './img' : './img' // 浏览器图片加载路径
-      }
-    }]
   },
   {
     test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
